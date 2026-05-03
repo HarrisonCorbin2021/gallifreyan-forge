2fc6dc885eeb2dba37909848a999cbbc46e24d70
corbinh <corbinh@mail.gvsu.edu>
2026-05-03 00:15:17 -0400

    First Custom Item

--- src/main/java/net/pencilshavings/gallifreyanforge/GallifreyanForge.java
+++ src/main/java/net/pencilshavings/gallifreyanforge/GallifreyanForge.java
@@ -1,6 +1,8 @@
 package net.pencilshavings.gallifreyanforge;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -13,6 +15,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.pencilshavings.gallifreyanforge.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -31,6 +34,8 @@ public class GallifreyanForge
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
@@ -45,7 +50,9 @@ public class GallifreyanForge
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.TARANIUM);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/pencilshavings/gallifreyanforge/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.pencilshavings.gallifreyanforge.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.pencilshavings.gallifreyanforge.GallifreyanForge;
+
+public class ModItems
+{
+    public static final DeferredRegister<Item>  ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, GallifreyanForge.MOD_ID);
+
+    public static final RegistryObject<Item> TARANIUM = ITEMS.register("taranium",
+            () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
